8ff088b47db1b5f5c507081aeebcd637c8ffd504
Leo Okawa Eriscon <leo.ericson@yahoo.se>
2017-06-27 15:58:11 +0200

    Add error handling for registration

--- passport/register.js
+++ passport/register.js
@@ -5,17 +5,21 @@
 const User     = require('../models/user'),
       passport = require('passport');
 
-module.exports = function handleRegistration(req, res){
-    req.logout(); 
+module.exports = function (req, res, next){
+    req.logout();
     console.log('New registration incoming!');
     console.log(req.body);
     User.register(new User({ username: req.body.username }), req.body.password, function( err, account){
         if(err){
             console.log('Error registering user!');
-            return res.render('register', {account: account});
+            return res.render('register', {account: account, error: err.message});
         }
 
-        passport.authenticate('local')(req, res, function(){
+        passport.authenticate('local')(req, res, function(err){
+            if (err) {
+                return next(err);
+            }
+
             console.log('User registered!');
             res.redirect('/');
         });
