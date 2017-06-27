aca59b754d682389808593dfc6a47ecff3d23fd1
Leo Okawa Eriscon <leo.ericson@yahoo.se>
2017-06-27 13:29:38 +0200

    [WIP] Working example of login features
    This is by no means final but it is working

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 /*jslint node: true*/
 /*jslint esversion: 6*/
 'use strict';
-const app      = require('express')(),
-      http     = require('http'),
-      url      = require('url'),
-      mongoose = require('mongoose'),
-      socketio = require('socket.io'),
-      server   = http.createServer(app),
-      io       = socketio(server),
-      port     = 8080;
+const app           = require('express')(),
+      bodyparser    = require('body-parser'),
+      cookieParser  = require('cookie-parser'),
+      http          = require('http'),
+      url           = require('url'),
+      mongoose      = require('mongoose'),
+      socketio      = require('socket.io'),
+      passport      = require('passport'),
+      LocalStrategy = require('passport-local'),
+      server        = http.createServer(app),
+      io            = socketio(server),
+      port          = 8080;
 
-mongoose.connect('mongodb://localhost:/test');//Connect to database 'test'
+mongoose.connect('mongodb://localhost:/Users');//Connect to database 'test'
 let db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Database connection error'));
@@ -22,9 +26,53 @@ db.once('open', () =>{
 
 app.set('views', './views');//Set directory for views
 app.set('view engine', 'ejs');
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
 app.use('/', require('./routes/index'));
+
+// Passport config
+let User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Catch 404 and forward to error handler
+app.use( (req, res, next) => {
+    let err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+//production error handler will print stacktrace
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
+});
+
+// develpment error handler
+// will print stacktrace
+
+// app.post('/login',
+//     passport.authenticate('local', { failureRedirect: '/login' }),
+//     (req, res) => {
+//      res.redirect('/');
+//     });
 
 
 server.listen(port, () => {
-    console.log('Listening on: ' + server.adress().port);
+    console.log('Listening on: ' + server.address().port);
 });
