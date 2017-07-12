051f175501eca42f0c5b7d8b680190aa61eaf38c
Leo Okawa Eriscon <leo.ericson@yahoo.se>
2017-07-12 19:45:57 +0200

    [Test] Check if the correct number of input-boxes are rendered

---
+++ react-frontend/src/components/InputContainer.test.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {InputContainer, InputBox } from './InputContainer.jsx'
+import renderer from 'react-test-renderer';
+
+test('number of boxes equal to synonyms', ()=> {
+  let answer = {
+    synonyms: [
+      {
+        alternatives: [
+          {text: "hej"},
+          {text: "hejsan"}
+        ]
+      },
+      {
+        alternatives: [
+          {text: "god kväll"},
+          {text: "god afton"}
+        ]
+      }
+    ]
+  } 
+
+  const component = renderer.create(
+    <InputContainer numberOfBoxes={ answer.synonyms.length }/>
+  )
+
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+  answer = {
+    synonyms: [
+      {
+        alternatives: [
+          {text: "hallao"},
+          {text: "nej"}
+        ]
+      },
+      {
+        alternatives: [
+          {text: "hallao"},
+          {text: "hejsan"}
+        ]
+      },
+      {
+        alternatives: [
+          {text: "hej"},
+          {text: "hejsan"}
+        ]
+      },
+      {
+        alternatives: [
+          {text: "god kväll"},
+          {text: "god afton"}
+        ]
+      }
+    ]
+  } 
+
+  const component1 = renderer.create(
+    <InputContainer numberOfBoxes={ answer.synonyms.length }/>
+  )
+
+  let tree = component1.toJSON();
+  expect(tree).toMatchSnapshot();
+});
