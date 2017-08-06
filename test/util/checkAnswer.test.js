62348c0bada409440222b0b600ccc0eed7e50fb9
Leo Okawa Eriscon <leo.ericson@yahoo.se>
2017-08-06 18:05:33 +0200

    [Test]Make mocha tests recursive

---
+++ test/util/checkAnswer.test.js
@@ -0,0 +1,34 @@
+/*jslint node: true*/
+/*jslint esversion: 6*/
+'use strict';
+
+
+let checkMatch = require('../../util/checkAnswer.js').checkMatch;
+let assert = require('assert')
+
+describe('checkMatch',  () => {
+  it('', ()=> {
+    let answer = {
+      synonyms: [
+        {
+          alternatives: [
+            {text: "hej"},
+            {text: "hejsan"}
+          ]
+        },
+        {
+          alternatives: [
+            {text: "god kväll"},
+            {text: "god afton"}
+          ]
+        }
+      ]
+    };
+
+    let wrong = 'no';
+    let correct = 'hej';
+
+    assert.equal(checkMatch(wrong, answer), -1);
+    assert.equal(checkMatch(correct, answer), 0);
+  })
+})
